e5222e8129514f400bc05c35e76d2c71977392e0
Tharun Madamshetti <tmadamshetti@delta.local>
2024-06-05 08:50:20 +0530

    added authenticator

--- amplify/backend/function/amplifyseslam/src/index.js
+++ amplify/backend/function/amplifyseslam/src/index.js
@@ -1,17 +1,41 @@
 
 
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+
+    import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+    export const handler = async (event) => {
+      // TODO implement
+      try{
+      const email= event.emailid;
+      const bodytext= event.body;
+      const subject=event.subject;
+      
+      const ses = new SESClient({ region: "ap-south-1" });
+       const command = new SendEmailCommand({
+        Destination: {
+          ToAddresses: [email]
+        },
+        Message: {
+          Body: {
+            Text: { Data: bodytext },
+          },
+    
+          Subject: { Data: subject },
+        },
+        Source: "tarun621997@gmail.com",
+      });
+      await ses.send(command);
+    
+      
+      return "ok";
+      }
+      catch(errorMessage){
+      //errorMessage.status(400);
+      return "not ok";
+      }
     };
-};
+    
