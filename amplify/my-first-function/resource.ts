2ac4c989ea741ff75532527663d8972bc8a4f448
Tharun Madamshetti <tmadamshetti@delta.local>
2024-06-05 15:28:08 +0530

    added authenticator

--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -1,4 +1,6 @@
 import { defineFunction } from '@aws-amplify/backend';
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "ap-south-1" });
 
 export const sayHello = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
